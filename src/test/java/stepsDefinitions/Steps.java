d2cf6a89e64fd42b785096555b82162990d3eadf
unknown <krzysztof.michalak@setapp.pl>
2020-07-05 11:35:52 +0200

    Add chrome driver to project

--- src/test/java/stepsDefinitions/Steps.java
+++ src/test/java/stepsDefinitions/Steps.java
@@ -3,28 +3,33 @@ package stepsDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Steps {
 
-    @Given("warunki początkowe")
+    WebDriver driver;
+
+    @Given("a user is on google.com page")
     public void precondition()
     {
-        System.out.println(">>> Warunki początkowe");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
-    @When("krok 1")
+    @When("the user types sda in google input search")
     public void step1()
     {
         System.out.println(">>> Wykonanie korku 1");
     }
 
-    @When("krok 2")
+    @When("the user clicks on search button")
     public void step2()
     {
         System.out.println(">>> Wykonanie korku 2");
     }
 
-    @Then("sprawdzenie")
+    @Then("the user should see sdacademy on search results")
     public void sprawdzenie()
     {
         System.out.println(">>> Sprawdzenie wyników");
--- src/test/testFeature.feature
+++
@@ -1,7 +0,0 @@
-Feature: to jest nasz testowy feature
-
-  Scenario: testowy scenario
-    Given warunki początkowe
-    When krok 1
-      And krok 2
-    Then sprawdzenie
